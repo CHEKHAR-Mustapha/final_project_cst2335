270e83b03ce806e4914a393aa2d06b4ff57a11e8
Chen Wang <cwang646@gmail.com>
2025-08-04 15:44:09 -0400

    some error on web application, but able to run on phone. edit from airplane branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,104 @@
 import 'package:flutter/material.dart';
-import 'airplane_page.dart';
+import 'flight_list_dao.dart';
+import 'flight_list_db.dart';
+import 'flight_list_page.dart';
+import 'encrypted_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the databases
+  final flightDatabase = await $FloorFlightDatabase.databaseBuilder('flight_database.db').build();
+  final flightDao = flightDatabase.flightDao;
+
+  // Initialize EncryptedPreferences
+  final encryptedPrefs = EncryptedPreferences();
+
+  runApp(MyApp(
+    flightDao: flightDao,
+    encryptedPrefs: encryptedPrefs,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FlightDao flightDao;
+  final EncryptedPreferences encryptedPrefs;
+  
+  const MyApp({
+    super.key,
+    required this.flightDao,
+    required this.encryptedPrefs
+  });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Airplane Manager',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const AirplanePage(),
-      debugShowCheckedModeBanner: false,
+      title: 'Airplane Management',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(
+        flightDao: flightDao,
+        encryptedPrefs: encryptedPrefs,
+      ),
+      routes: {
+        '/homePage': (context) => MyApp(
+          flightDao: flightDao,
+          encryptedPrefs: encryptedPrefs,
+        ),
+      },
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  final FlightDao flightDao;
+  final EncryptedPreferences encryptedPrefs;
+
+  const MyHomePage({
+    super.key,
+    required this.flightDao,
+    required this.encryptedPrefs,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton.icon(
+              onPressed: _navigateToFlightListPage,
+              icon: const Icon(Icons.flight),
+              label: const Text('Go to Flight List Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _navigateToFlightListPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FlightListPage(
+          flightDao: widget.flightDao,
+          encryptedPrefs: widget.encryptedPrefs,
+        ),
+      ),
+    );
+  }
+
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:final_project_cst2335/airplane_page.dart';
-
+import 'package:final_project_cst2335/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
